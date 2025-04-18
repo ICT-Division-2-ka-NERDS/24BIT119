e6e33e31c1d9e4c1fdd23dda774b129a75f5e88a
MeghPatel327 <meghp706@gmail.com>
2025-04-18 17:33:29 +0530

    Add files via upload

---
+++ 09/07.py
@@ -0,0 +1,7 @@
+# 24bit119
+
+# Que-7
+def ispalindrome(s):
+    return s == s[::-1]
+
+print(ispalindrome(input("Enter string: ")))
---
+++ 09/08.py
@@ -0,0 +1,10 @@
+# 24bit119
+
+# Que-8
+def convert():
+    s = input("Enter string here: ")
+    l = s.split()
+    l = list(set(l))
+    l.sort()
+    print(l)
+convert()
