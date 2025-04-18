d719dc0062cfccf0cd58c59fa5993e53faaf9ca5
MeghPatel327 <meghp706@gmail.com>
2025-04-18 17:04:19 +0530

    Delete 07 directory

--- 07/02.py
+++
@@ -1,8 +0,0 @@
-# Que-2
-
-d = {}
-
-if not d:
-    print("Dictionary is empty")
-else:
-    print("Dictionary is not empty")
--- 07/04.py
+++
@@ -1,7 +0,0 @@
-# Que-4
-
-s = input("Enter a string: ")
-d={}
-for i in s:
-    d[i] = s.count(i)
-print(d)
